c4a3e49558099cc551d0a8ff4d19b68f4e464b42
Abhijitam01 <abhijitamdubey31@gmail.com>
2026-01-18 20:44:58 +0530

    chore(web): add eslint configuration

---
+++ apps/web/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ['next/core-web-vitals', '../../packages/config/eslint/base.js'],
+    parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+    },
+};
